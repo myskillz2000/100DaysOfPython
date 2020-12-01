bcb4132c5a0e83a507ff3263e5aa8d3ab636dc28
Matt Stiller <Myskillz2000@gmail.com>
2020-12-01 18:18:36 +0000

    Day two exercises with data types.

---
+++ Day2/exercise.py
@@ -0,0 +1,9 @@
+#Work with some data types in python
+two_digit_number = input("Type a two digit number: ")
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+
+answer = first_num + second_num
+
+print(str(first_num) + " + " + str(second_num) + " is equal to " + str(answer))
