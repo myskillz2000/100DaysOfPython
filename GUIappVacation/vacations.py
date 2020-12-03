03dda222ce74dce17779c36b7d960ea1676b96f9
Matt Stiller <matthewstiller484@yahoo.com>
2020-12-03 16:31:47 -0500

    Started an desktop application to track vacations spots.

---
+++ GUIappVacation/vacations.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 14:35:08 2020
+
+@author: Owner
+"""
+
+
+#Create a tkinter application for vactions I would like to take.
+
+from tkinter import *
+
+
