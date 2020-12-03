53d7b1795dc4224121b1c36065e7c7e52fe87c18
Matt Stiller <Myskillz2000@gmail.com>
2020-12-03 00:20:28 +0000

    Added some examples of how different data types work in python.

--- Day2/exercise.py
+++ Day2/exercise.py
@@ -7,3 +7,10 @@ second_num = int(two_digit_number[1])
 answer = first_num + second_num
 
 print(str(first_num) + " + " + str(second_num) + " is equal to " + str(answer))
+
+#Division makes the data type a float
+num = 6/3
+print(num)
+
+#Python follows PEMDAS rule here is an example.
+print(3 *( 3 + 3) / 3 - 3**3)
